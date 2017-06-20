dcff22af4477d538809e2be9e8489080076510cc
Guillaume Limberger <glim.dev@gmail.com>
2017-06-20 20:17:51 +0200

    [Back] A Range object now contains a array of notes

--- back/src/AppBundle/Entity/Range.php
+++ back/src/AppBundle/Entity/Range.php
@@ -14,14 +14,14 @@ use Doctrine\ORM\Mapping as ORM;
 final class Range
 {
     const VALUES = [
-        'C0', 'D0', 'E0', 'F0', 'G0', 'A0', 'B0',
-        'C1', 'D1', 'E1', 'F1', 'G1', 'A1', 'B1',
-        'C2', 'D2', 'E2', 'F2', 'G2', 'A2', 'B2',
-        'C3', 'D3', 'E3', 'F3', 'G3', 'A3', 'B3',
-        'C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4',
-        'C5', 'D5', 'E5', 'F5', 'G5', 'A5', 'B5',
-        'C6', 'D6', 'E6', 'F6', 'G6', 'A6', 'B6',
-        'C7', 'D7', 'E7', 'F7', 'G7', 'A7', 'B7',
+        'C0', 'C0#', 'D0', 'E0b', 'E0', 'F0', 'F0#', 'G0', 'G0#', 'A0', 'B0b', 'B0',
+        'C1', 'C1#', 'D1', 'E1b', 'E1', 'F1', 'F1#', 'G1', 'G1#', 'A1', 'B1b', 'B1',
+        'C2', 'C2#', 'D2', 'E2b', 'E2', 'F2', 'F2#', 'G2', 'G2#', 'A2', 'B2b', 'B2',
+        'C3', 'C3#', 'D3', 'E3b', 'E3', 'F3', 'F3#', 'G3', 'G3#', 'A3', 'B3b', 'B3',
+        'C4', 'C4#', 'D4', 'E4b', 'E4', 'F4', 'F4#', 'G4', 'G4#', 'A4', 'B4b', 'B4',
+        'C5', 'C5#', 'D5', 'E5b', 'E5', 'F5', 'F5#', 'G5', 'G5#', 'A5', 'B5b', 'B5',
+        'C6', 'C6#', 'D6', 'E6b', 'E6', 'F6', 'F6#', 'G6', 'G6#', 'A6', 'B6b', 'B6',
+        'C7', 'C7#', 'D7', 'E7b', 'E7', 'F7', 'F7#', 'G7', 'G7#', 'A7', 'B7b', 'B7',
     ];
 
     /**
@@ -71,20 +71,34 @@ final class Range
      */
     private function __construct(array $range)
     {
-        $this->range = $range;
+        $values = [];
+
+        list($lowerLimit, $upperLimit) = $range;
+        $lowerIndex = array_search($lowerLimit, self::VALUES);
+        $upperIndex = array_search($upperLimit, self::VALUES);
+
+        for ($i = $lowerIndex; $i <= $upperIndex; $i++ ) {
+            $values[] = self::VALUES[$i];
+        }
+        $this->range = $values;
     }
 
     public function getLowerLimit(): string
     {
-        list($lowerLimit,) = $this->range;
-
-        return $lowerLimit;
+        return reset($this->range);
     }
+
 
     public function getUpperLimit(): string
     {
-        list(,$upperLimit) = $this->range;
+        return end($this->range);
+    }
 
-        return $upperLimit;
+    /**
+     * @return array
+     */
+    public function getValues(): array
+    {
+        return $this->range;
     }
 }
--- back/tests/AppBundle/Entity/RangeTest.php
+++ back/tests/AppBundle/Entity/RangeTest.php
@@ -49,4 +49,19 @@ class RangeTest extends TestCase
     {
         $range = Range::create(['C3', 'D1']);
     }
+
+    public function testValues()
+    {
+        $range = Range::create(['C1', 'C2']);
+
+        $expectedValues = ['C1', 'C1#', 'D1', 'E1b', 'E1', 'F1', 'F1#', 'G1', 'G1#', 'A1', 'B1b', 'B1', 'C2'];
+        $this->assertSame($expectedValues, $range->getValues());
+    }
+
+    public function testOneValue()
+    {
+        $range = Range::create(['C1', 'C1']);
+
+        $this->assertSame(['C1'], $range->getValues());
+    }
 }
