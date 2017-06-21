7490e644e701963a38126e7180d31739fc70daef
Guillaume Limberger <glim.dev@gmail.com>
2017-06-21 22:17:40 +0200

    [Back] Register VichUploaderBundle bundle

--- back/app/AppKernel.php
+++ back/app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Vich\UploaderBundle\VichUploaderBundle(),
             new AppBundle\AppBundle(),
         ];
 
