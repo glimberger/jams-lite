5ce8add1dbffd34cc70e93c12b24bd47891db1b8
Guillaume Limberger <glim.dev@gmail.com>
2017-06-21 22:13:26 +0200

    [Back] Install VichUploaderBundle bundle and use it in Sample class

--- back/src/AppBundle/Entity/Sample.php
+++ back/src/AppBundle/Entity/Sample.php
@@ -3,7 +3,10 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Entity\File as EmbeddedFile;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * Class Sample
@@ -25,24 +28,31 @@ final class Sample
     private $id;
 
     /**
-     * @var string
+     * @var EmbeddedFile
+     *
+     * @ORM\Embedded(class="Vich\UploaderBundle\Entity\File")
      */
-    private $soundName;
-
-    /**
-     * @var string
-     */
-    private $soundOriginalName;
-
-    /**
-     * @var int
-     */
-    private $soundSize;
+    private $sound;
 
     /**
      * @var UploadedFile
+     *
+     * @Vich\UploadableField(
+     *     mapping="sample",
+     *     fileNameProperty="sound.name",
+     *     mimeType="sound.mimeType",
+     *     originalName="sound.originalName",
+     *     size="sound.size"
+     * )
      */
     private $soundFile;
+
+    /**
+     * @ORM\Column(type="datetime")
+     *
+     * @var \DateTime
+     */
+    private $updatedAt;
 
     /**
      * Track constructor.
@@ -52,6 +62,7 @@ final class Sample
     public function __construct(?int $id)
     {
         $this->id = $id;
+        $this->sound = new EmbeddedFile();
     }
 
     /**
@@ -60,5 +71,50 @@ final class Sample
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return File|null
+     */
+    public function getSoundFile(): File
+    {
+        return $this->soundFile;
+    }
+
+    /**
+     * @param null|File|UploadedFile $sound
+     *
+     * @return Sample
+     */
+    public function setSoundFile(?File $sound = null): Sample
+    {
+        $this->soundFile = $sound;
+
+        if ($sound) {
+            // It is required otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return EmbeddedFile
+     */
+    public function getSound(): EmbeddedFile
+    {
+        return $this->sound;
+    }
+
+    /**
+     * @param EmbeddedFile $sound
+     *
+     * @return Sample
+     */
+    public function setSound(EmbeddedFile $sound): Sample
+    {
+        $this->sound = $sound;
+
+        return $this;
     }
 }
